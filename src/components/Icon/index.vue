60ae03af7364ac05c27681e337104e37ceb75094
Christian Toledo <chrstiantoledo@gmail.com>
2021-02-09 12:33:16 -0300

    creat icon component

---
+++ src/components/Icon/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <component :is="name" v-bind="$props" />
+</template>
+
+<script>
+import Logo from './Logo.vue'
+export default {
+  components: { Logo },
+  props: {
+    name: { type: String, required: true }
+  }
+}
+</script>
