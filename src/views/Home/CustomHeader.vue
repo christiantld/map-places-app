7484411ca051a97118bca0878a9812ce1e1d66b2
Christian Toledo <chrstiantoledo@gmail.com>
2021-02-09 12:34:42 -0300

    create home view

---
+++ src/views/Home/CustomHeader.vue
@@ -0,0 +1,88 @@
+<template>
+  <header class="header">
+    <div class="header-group">
+      <div
+        class="flex items-center justify-between py-10 border-b-2 border-brand-navyblue w-full"
+      >
+        <div class="w-28 lg:w-36">
+          <icon
+            name="logo"
+            size="35"
+            :color="brandColors.navyblue"
+            alt="logo"
+            class="w-full"
+          />
+        </div>
+        <div class="flex">
+          <button
+            class="rounded-full text-brand-darkinfo hover:text-brand-success custom-btn"
+            @click="() => emit('create-account')"
+          >
+            Crie uma conta
+          </button>
+          <button
+            @click="() => emit('login')"
+            class="bg-brand-navyblue rounded-full text-brand-gray hover:bg-brand-success custom-btn"
+          >
+            Entrar
+          </button>
+        </div>
+      </div>
+
+      <div class="flex flex-col mt-28">
+        <h1 class="text-4xl font-black text-brand-navyblue">
+          Encontre locais<br />
+          próximos a você.
+          <br class="hidden lg:inline-block" />
+        </h1>
+        <p class="text-lg font-medium pt-5 text-brand-darkinfo">
+          Explore,favorite, avalie e compartilhe
+          <br class="hidden lg:inline-block" />
+          seus locais com outros usuários.
+        </p>
+        <div>
+          <button
+            @click="() => emit('create-account')"
+            class="mt-10 bg-brand-navyblue rounded-full text-brand-gray hover:bg-brand-success custom-btn"
+          >
+            Crie sua conta agora
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+import Icon from '@/components/Icon'
+import palette from '../../../palette'
+
+export default {
+  components: { Icon },
+  setup(_, { emit }) {
+    return { emit, brandColors: palette.brand }
+  }
+}
+</script>
+<style lang="postcss" scoped>
+.header {
+  @apply bg-white w-full flex justify-center;
+  height: 700px;
+}
+
+.header-group {
+  @apply flex flex-col w-4/5 max-w-6xl;
+}
+
+.custom-btn {
+  @apply px-6 py-2 font-bold focus:outline-none transition duration-300;
+}
+
+@media (min-width: 1079px) {
+  .header-group {
+    background-image: url('../../assets/images/ocean.png');
+    background-size: 628px;
+    background-position: 100% 80%;
+    background-repeat: no-repeat;
+  }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,10 +1,36 @@
 <template>
   <div>
-    <h1 class="text-brand-main">Home</h1>
+    <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
+    <div
+      class="flex justify-center py-10 bg-brand-navyblue absolute bottom-0 w-full"
+    >
+      <p class="font-medium text-center text-brand-gray">
+        Christian Toledo 2021 - Made with 💙 and ☕
+      </p>
+    </div>
   </div>
 </template>
 <script>
-export default {}
+import CustomHeader from './CustomHeader.vue'
+import useModal from '@/hooks/useModal'
+
+export default {
+  components: { CustomHeader },
+  setup() {
+    const modal = useModal()
+    function handleLogin() {
+      modal.open({
+        component: 'ModalLogin'
+      })
+    }
+
+    function handleAccountCreate() {
+      modal.open({
+        component: 'ModalAccountCreate'
+      })
+    }
+
+    return { handleLogin, handleAccountCreate }
+  }
+}
 </script>
-<style lang="">
-</style>
