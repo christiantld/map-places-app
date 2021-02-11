2125e63a7f0a62b2fe4a6e37e52de3dac44ea6eb
Christian Toledo <chrstiantoledo@gmail.com>
2021-02-11 00:25:05 -0300

    ufeat: add HeaderLogged component and minor css updates

--- src/views/Discovery/index.vue
+++ src/views/Discovery/index.vue
@@ -1,8 +1,14 @@
 <template>
+  <div class="flex justify-center w-full h-28 bg-brand-navyblue">
+    <header-logged />
+  </div>
   <div>Discovery</div>
 </template>
 <script>
-export default {}
+import HeaderLogged from '@/components/HeaderLogged'
+export default {
+  components: { HeaderLogged }
+}
 </script>
 <style lang="">
 </style>
