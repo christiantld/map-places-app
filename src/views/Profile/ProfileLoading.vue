4277985d51ba0e030c03b38e1addb3060eebaa12
Christian Toledo <chrstiantoledo@gmail.com>
2021-02-11 17:43:22 -0300

    feat: add contentLoader component

---
+++ src/views/Profile/ProfileLoading.vue
@@ -0,0 +1,38 @@
+<template>
+  <content-loader
+    class="flex flex-col items-center rounded mt-2"
+    width="60%"
+    height="470px"
+    animation-duration="3s"
+  >
+    <content-loader class="my-5 rounded-full" width="9rem" height="9rem" />
+    <content-loader
+      class="mt-4 rounded"
+      width="90%"
+      height="75px"
+      animation-duration="2s"
+    />
+
+    <content-loader
+      class="mt-4 rounded"
+      width="90%"
+      height="75px"
+      animation-duration="2.2s"
+    />
+
+    <content-loader
+      class="my-4 rounded"
+      width="90%"
+      height="75px"
+      animation-duration="2.4s"
+    />
+  </content-loader>
+</template>
+
+<script>
+import ContentLoader from '@/components/ContentLoader'
+
+export default {
+  components: { ContentLoader }
+}
+</script>
