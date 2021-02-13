7e9ace0a7507023dfd9a867ed94da8f2b0fc922f
Christian Toledo <chrstiantoledo@gmail.com>
2021-02-12 21:28:10 -0300

    add photo icon

--- src/components/Icon/index.vue
+++ src/components/Icon/index.vue
@@ -5,8 +5,9 @@
 <script>
 import Logo from './Logo.vue'
 import Loading from './Loading.vue'
+import Photo from './Photo.vue'
 export default {
-  components: { Logo, Loading },
+  components: { Logo, Loading, Photo },
   props: {
     name: { type: String, required: true }
   }
