541fc223aae628a5cd38474d663e39dcdf256073
Christian Toledo <chrstiantoledo@gmail.com>
2021-02-15 17:40:32 -0300

    feat: add Mpa and MapLoader components

---
+++ src/views/Discovery/MapLoader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="map flex justify-center items-center z-1">
+    <img src="../../assets/icons/map.svg" class="w-64" alt="" />
+  </div>
+</template>
+
+<script>
+import Icon from '@/components/Icon'
+export default {
+  component: { Icon },
+  name: 'MapLoader'
+}
+</script>
+
+<style lang="css" scoped>
+.map {
+  width: 100%;
+  height: 87vh;
+}
+</style>
