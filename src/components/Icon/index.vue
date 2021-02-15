ecc76e5f6fa4b260c25430bbe75caa1ca1ef2ca3
Christian Toledo <chrstiantoledo@gmail.com>
2021-02-15 17:37:26 -0300

    feat: add star, star-stroke and place icons

--- src/components/Icon/index.vue
+++ src/components/Icon/index.vue
@@ -6,8 +6,11 @@
 import Logo from './Logo.vue'
 import Loading from './Loading.vue'
 import Photo from './Photo.vue'
+import Place from './Place.vue'
+import Star from './Star'
+import StarStroke from './StarStroke'
 export default {
-  components: { Logo, Loading, Photo },
+  components: { Logo, Loading, Photo, Place, Star, StarStroke },
   props: {
     name: { type: String, required: true }
   }
